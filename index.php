144f4386b640fb7ef92ae2aa5575c0d31510e1f7
taotara <tatoara@yahoo.com>
2021-07-29 12:50:29 +0100

    added an add page

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
 <!DOCTYPE html>
 <html lang="en">
     <?php include('templates/header.php'); ?>
-
-    <?php include('templates/footer.php'); ?>
     
-
+    <?php include('templates/footer.php'); ?>
 </html>
