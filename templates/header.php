c871554ff524bebbdd88ccd7367a78502b6dc5d3
taotara <tatoara@yahoo.com>
2021-07-29 11:58:57 +0100

    My first commit

---
+++ templates/header.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ninja Pizza</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="templates/style.css">
+</head>
+<body class="grey lighten-4">
+    <nav class="white z-depth-0">
+        <div class="container">
+            <a href="#" class="brand-logo brand-text">Ninja Pizza</a>
+            <ul id="nav-mobile" class="right hide-on-small-and-down">
+                <li> <a href="#" class="btn brand z-dept-0">Add a piza</a> </li>
+            </ul>
+        </div>
+    </nav>
